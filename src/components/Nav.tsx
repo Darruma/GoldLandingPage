edc6e405e9dc5d5d2a78595898963d31ecca10f2
Umair Darr <umerderr@gmail.com>
2023-09-19 22:18:14 +0100

    feat: add stuff

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,29 @@
+import { PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+function NavLink( {to, children}: PropsWithChildren<{to: string}> ) {
+  return (
+    <Link to={to} className="font-bold text-2xl">
+      {children}
+    </Link>
+  );
+}
+
 function Nav() {
   return (
     <div>
       <div className="bg-black h-20"></div>
       <div className="p-2"></div>
       <div className="bg-black p-1 hidden md:flex"></div>
+      <div className="p-2 hidden md:flex flex-row gap-4 ml-16">
+        <img src={logo} alt="logo" className="h-50 z-10 mt-[-4rem]" />
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/swap">Buy</NavLink>
+        <NavLink to="/stake">Swap</NavLink>
+        <NavLink to="/learn">Learn</NavLink>
+        
+      </div>
     </div>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,30 @@
 import Nav from "../components/Nav";
-
+import bar from "../assets/bar.png";
 function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-full overflow-hidden">
       <Nav />
-      <div>
-        <h1>HomePage Contnet</h1>
+      <div className="mx-16 basis-full">
+        <div className="flex flex-row items-center md:h-full">
+
+
+        <div className="flex flex-col gap-4 justify-center items-center md:pt-0 pt-32 basis-full">
+          <h1 className="press-start-2p text-secondary text-4xl text-center">GOLDEN BOYS</h1>
+          <div className="font-semi-bold text-2xl text-center">The $GOLD Standard for DeFi on Base</div>
+          <div className="flex flex-row justify-center items-center gap-4">
+            <a href="/buy" className="bg-secondary py-2 w-48 text-center rounded-lg text-black font-bold text-2xl">
+                Buy
+            </a>
+            <a href="chart" className="bg-secondary py-2 w-48 text-center rounded-lg text-black font-bold text-2xl">
+                  Chart
+            </a>
+          </div>
+
+        </div>
+        <div className="basis-full h-full w-full hidden md:flex justify-center ">
+        <img src={bar} alt="bar" className="h-50 z-10 " />
+        </div>
+        </div>
       </div>
     </div>
   );
