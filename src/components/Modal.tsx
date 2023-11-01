808997e34a4c1ec448d1e17e5440033be99639e7
Darruma <umerderr@gmail.com>
2023-11-01 18:41:44 +0000

    feat: update modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -39,7 +39,9 @@ function Modal({ close, auraPrice, auraToGoldAuraRatio, action }: ModalProps) {
           <div className="flex flex-row justify-between">
             <div className="p-4 border  border-amber-300 rounded-md bg-[#2b2b2b] flex flex-row items-center gap-2 basis-full ">
               <img src={Aura}></img>
-              <div className="text-gray-400 pr-8">Aura</div>
+              <div className="text-gray-400 pr-8">
+                {isDeposit ? "Aura" : "goldAura"}
+              </div>
             </div>
             <div className="flex flex-col">
               <input
@@ -79,7 +81,9 @@ function Modal({ close, auraPrice, auraToGoldAuraRatio, action }: ModalProps) {
           <div className="flex flex-row justify-between">
             <div className="p-4 border  border-amber-300 rounded-md bg-[#2b2b2b] flex flex-row items-center gap-2 basis-full ">
               <img src={Aura}></img>
-              <div className="text-gray-400 pr-8">goldAura</div>
+              <div className="text-gray-400 pr-8">
+                {isDeposit ? "goldAura" : "Aura"}
+              </div>
             </div>
             <div className="basis-full flex flex-col gap-2 text-right text-gray-400">
               <div className="basis-full text-gray-400 bg-black text-right text-2xl">
