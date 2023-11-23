c3ea9beb4697b4c2b058f0bc68f80589f6b342de
Darruma <umerderr@gmail.com>
2023-11-23 23:34:26 +0000

    feat: add vault link

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -364,6 +364,56 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="p-20 flex flex-col">
+        <div className="md:px-40">
+          <div className="font-bold md:text-6xl text-4xl text-center ">
+            How to buy <span className="text-secondary">$GOLD</span> & What to
+            do with it
+          </div>
+          <br />
+          add image here with steps (also needed for mobile)
+          <br />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row md:items-center bg-[#292E41] w-4/5 h-[350px] mx-auto gradient-border justify-evenly gap-2 pt-12 ">
+        <div className="basis-full flex flex-col justify-center items-center gap-4 ">
+          <div className="font-bold md:text-5xl text-4xl text-center">
+            Earn passive income with GoldenBoys' goldAura Vault
+          </div>
+          <div className="text-center fira text-xl">
+            Deposit AURA and earn in comfort
+          </div>
+          <a
+            href="/#/vault"
+            className="bg-tertiary text-white w-64 py-2 text-center cursor-pointer"
+          >
+            Enter the Vault
+          </a>
+        </div>
+        <div className="basis-full flex justify-center items-center relative pt-32 mb-12">
+          <img
+            className="absolute left-[30%] z-10"
+            src={gold_small}
+            alt="bars"
+            width={150}
+            height={500}
+          />
+          <img
+            className="absolute bottom-[10%]"
+            src={gold_small}
+            alt="bars"
+            width={150}
+            height={500}
+          />
+          <img
+            className="absolute right-[30%]"
+            src={gold_small}
+            alt="bars"
+            width={150}
+            height={500}
+          />
+        </div>
+      </div>
     </div>
   );
 }
