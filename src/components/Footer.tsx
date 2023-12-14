29bed6fcf4baaead8253e0c25574ab6dcf748208
Darruma <umerderr@gmail.com>
2023-12-14 17:39:18 +0000

    feat: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,13 +9,25 @@ export const Footer = () => {
       <img src={goldenLogo} alt="logo" />
 
       <div className="flex flex-row gap-4">
-        <a>
+        <a
+          href="https://discord.gg/Dr5T464wYA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={golddisc} alt="golddisc" width={50} height={50} />
         </a>
-        <a>
+        <a
+          href="https://twitter.com/Gold_On_Chain"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={goldx} alt="goldx" width={50} height={50} />
         </a>
-        <a>
+        <a
+          href="https://t.me/+YUKhdQVv8KBmMjNk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={goldtg} alt="goldtg" width={50} height={50} />
         </a>
       </div>
